e85104600a87e12ca4db9ac1bf5815d0a67bd690
Mike G <michael.pov.it@gmail.com>
2021-02-09 17:48:33 +0200

    experience changed 2

--- variables.php
+++ variables.php
@@ -9,7 +9,7 @@ $position = "DevOps | TechLead";
 $contacts = [
     "Location:" => "Ukraine, Odessa, 65012",
     "Email:" => "<a href='mailto: mike.gordievsky@gmail.com'>mike.gordievsky@gmail.com</a>",
-    "Skype:" => "mike.gordievsky@gmail.com",
+    "Skype:" => "<a href="skype:live:.cid.3d4de0dbb497155a?chat">Start Chat</a>",
     "Telegram:" => "<a href='https://t.me/Mikhail_or_Mike'>@Mikhail_or_Mike</a>",
 ];
 $technologies = [
@@ -31,15 +31,15 @@ $experience = [
     ],
     "Imonomy Interactive LTD / DevOps" => [
         "Date" => "November 2019 - February 2020, Odessa",
-        "Responsibilities:" => "Configuring the RTB-servers up; Bash scripts; Crontab; Docker; Jenkins; Replications; Agents; Web-servers, MySQL; Wordpress."
+        "Responsibilities:" => "Configuring the RTB-servers up; Bash scripts; Crontab; Docker; Replications; Agents; Web-servers (Apache&NGINX), MySQL; Wordpress, AWS"
     ],
-    "482.Solutions / PM, Web-consultant" => [
+    /*"482.Solutions / PM, Web-consultant" => [
         "Date" => "July 2019 - October 2019, Odessa",
         "Responsibilities:" => "Work under R&D and in-house projects of the company. Team managment, requirements collecting and analysing, solutions delivering."
     ],
     "Nikolaev Scientific and Educational Center for Adult Education / Deputy Director for Commercial Affairs" => [
         "Date" => "November 2019 - Present",
-        "Responsibilities" => "Assistance to the Director in organizing of company's events"
+        "Responsibilities:" => "Assistance to the Director in organizing of company's events"
     ],
     "Freelance / Web-developer" => [
         "Date" => "December 2010 - Present, Remotely",
@@ -62,7 +62,7 @@ $experience = [
     ],
     "GeeksForLess  / HTML Coder" => [
         "Date" => "September 2006 - August 2007, Mykolaiv",
-        "Responsibilities" => "Working on company's projects with HTML&CSS. All projects’ information protected by NDA."
+        "Responsibilities:" => "Working on company's projects with HTML&CSS. All projects’ information protected by NDA."
     ],
     /*"to be continued..." => ["in progress"]*/
 ];
